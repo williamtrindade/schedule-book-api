2ae72ee0e36286ccf386f0cd35e4f38db10433d0
william <williamtrindade777@gmail.com>
2019-08-13 00:11:53 -0300

    dia 0 - Migrations

--- agenda/app/User.php
+++ agenda/app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function atividades()
+    {
+        return $this->hasMany('App\Atividade');
+    }
 }
---
+++ agenda/database/migrations/2019_08_13_025824_create_atividades_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtividadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atividades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('data_de_inicio');
+            $table->date('data_de_prazo');
+            $table->date('data_de_conclusao');
+            $table->string('status');
+            $table->string('tituto');
+            $table->string('descricao');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atividades');
+    }
+}
